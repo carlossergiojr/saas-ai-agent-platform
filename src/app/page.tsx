9151c6277ac30cdf8a4495a2d30d5cc090ead877
Carlos lima <carlosslimajr@hotmail.com>
2025-07-26 15:35:12 -0300

    05: auth socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,18 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
-import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+import { HomeView } from "@/modules/home/ui/views/home-view";
 
 const Page = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  if (!!session) {
-    redirect("/");
+  if (!session) {
+    redirect("/sign-in");
   }
 
-  return <SignInView />;
+  return <HomeView />;
 };
 
 export default Page;
-
